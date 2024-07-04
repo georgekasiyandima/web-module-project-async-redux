817a3923ed43430d9b848ad1f0514884af0f3d01
George Kasiyandima <kasiyandimageorge@gmail.com>
2024-07-04 06:22:56 +0200

    updated App.js file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
+// src/App.js
 import React from 'react';
 import EuroData from './components/EuroData';
-import './App.css';
 
 const App = () => {
-    return (
-        <div>
-            <EuroData />
-        </div>
-    );
+  return (
+    <div className="App">
+      <h1>Euro 2024 App</h1>
+      <EuroData />
+    </div>
+  );
 };
 
 export default App;
