3d2f7075afaabff4c9535c9f71931e83f7ae7089
George Kasiyandima <kasiyandimageorge@gmail.com>
2024-07-04 07:15:56 +0200

    added API key in the request

--- src/euroActions.js
+++ src/euroActions.js
@@ -1,3 +1,4 @@
+// src/euroActions.js
 import axios from 'axios';
 
 export const FETCH_EURO_REQUEST = 'FETCH_EURO_REQUEST';
@@ -5,37 +6,41 @@ export const FETCH_EURO_SUCCESS = 'FETCH_EURO_SUCCESS';
 export const FETCH_EURO_FAILURE = 'FETCH_EURO_FAILURE';
 
 export const fetchEuroRequest = () => {
-    return {
-        type: FETCH_EURO_REQUEST,
-    };
+  return {
+    type: FETCH_EURO_REQUEST,
+  };
 };
 
-export const fetchEuroSuccess = () => {
-    return {
-        type: FETCH_EURO_SUCCESS,
-        payload: data,
-    };
+export const fetchEuroSuccess = (data) => {
+  return {
+    type: FETCH_EURO_SUCCESS,
+    payload: data,
+  };
 };
 
 export const fetchEuroFailure = (error) => {
-    return {
-        type: FETCH_EURO_FAILURE,
-        payload: error,
-    };
+  return {
+    type: FETCH_EURO_FAILURE,
+    payload: error,
+  };
 };
 
 export const fetchEuroData = () => {
-    return (dispatch) => {
-        dispatch(fetchEuroRequest());
-        axios
-          .get('https://rapidapi.com/yuvr99-WHTEITBQbOc/api/euro-20242/playground/apiendpoint_fb6de029-3579-4afd-a7ba-114dae25d5f7')
-          .then((response) => {
-            const data = response.data;
-            dispatch(fetchEuroSuccess(data));
-          })
-          .catch((error) => {
-            const errorMsg = error.message;
-            dispatch(fetchEuroFailure(errorMsg));
-          });
-    };
+  return (dispatch) => {
+    dispatch(fetchEuroRequest());
+    axios
+      .get('https://euro-20242.p.rapidapi.com/groups/%7BgroupId%7D/teams', {
+        headers: {
+          'Authorization': 'x-rapidapi-key: 51c399b3d3msh8cb5488a1b15698p12ddcajsn00f08368c2df' // Use correct authorization header
+        }
+      })
+      .then((response) => {
+        const data = response.data;
+        dispatch(fetchEuroSuccess(data));
+      })
+      .catch((error) => {
+        const errorMsg = error.message;
+        dispatch(fetchEuroFailure(errorMsg));
+      });
+  };
 };
